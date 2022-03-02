46236a6fda954ecb5312e3d0bdf86feeea1440ad
CinthiaVB <cinthiaveb@gmail.com>
2022-03-02 12:16:55 -0500

    Showing in console Method Sort

--- src/main.js
+++ src/main.js
@@ -1,31 +1,18 @@
-// import { orderData } from './data.js';
-import data from './data/pokemon/pokemon.js';
+import { sortData } from './data.js';
+
+import bigdata from './data/pokemon/pokemon.js';
+
+const sortPokemon = bigdata.pokemon;
+
 //Mostrando data en página principal
 
-const test = document.querySelector('#test')
-data.pokemon.forEach((pokemon) => {
-    let pokemonTest = document.createElement("div");
-    test.appendChild(pokemonTest);
-    pokemonTest.innerHTML = `<img src = ${pokemon.img}> ${pokemon.num} ${pokemon.name} ${pokemon.type}`
-
+const showData = document.querySelector('#show-data')
+bigdata.pokemon.forEach((pokemon) => {
+    let pokemonData = document.createElement("div");
+    pokemonData.innerHTML = `<img src = ${pokemon.img}> ${pokemon.num} ${pokemon.name} ${pokemon.type}`
+    showData.appendChild(pokemonData);
 });
 
-
-
-
-
-
-// Ordenando data alfabéticamente
-    /*orderData.sort((a, b) => {
-    const namePokemonOne = a.pokemon.name
-    const namePokemonTwo = b.pokemon.name
-    if (namePokemonOne < namePokemonTwo) {
-        return -1
-    }
-    if (namePokemonOne > namePokemonTwo) {
-        return 1
-    }
-    return 0;
-});*/
-
+const orderedResult = sortData(sortPokemon);    
+console.log(orderedResult);
 
